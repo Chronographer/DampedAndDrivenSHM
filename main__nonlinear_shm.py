5fb702dc34fe029d20db4c7452564184d4577e13
Daniel <djisenberg2015@gmail.com>
2020-02-21 10:56:38 -0500

    implemented ability to measure the period for a given set of initial conditions by averaging the time of a number of periods. The initial theta can be incremented optionally, however it currently breaks the calculation of the period immediately after the initial theta incremented. However, I just realized that I don't want these to be added together, as I am looking at the differences in the period based on initial angle (I think), and so averaging them together would destroy the information I am looking for.

--- main__nonlinear_shm.py
+++ main__nonlinear_shm.py
@@ -1,24 +1,25 @@
-import shm_eulercromer
-import shm_driven
 import matplotlib.pyplot as plt
 import numpy as np
+import shm_eulercromer
+import shm_driven
 import amplitude_dependant_anharmonic
+import periodCounter
 
-initialTheta = 0.0
+initialTheta = 0.2
 initialOmega = 0.0
 initialAlpha = 0.0
 initialTime = 0.0
 
-maxTheta = np.pi/4
+maxTheta = 0.31
 thetaIncrement = 0.1
 timeStep = 0.005
-maxTime = 800.0
+maxTime = 20.0
 gravity = 9.8
 pendulumLength = 1.0
 mass = 1.0
 dragCoefficient = 0.0
 drivingForce = 0.0
-drivingFrequency = 2.35
+drivingFrequency = 0.0
 plotStartTime = 0  # the time when the first point will be plotted on the graph
 clamp = True
 plotType = "angle"
@@ -40,7 +41,7 @@ elif plotType == "phaseSpace":
     yAxisLabel = "Velocity (m/s)"
     xAxisLabel = "Angle (rad)"
 
-amplitude_dependant_anharmonic.run(gravity, pendulumLength, initialTheta, maxTheta, thetaIncrement, initialOmega, initialTime, timeStep, maxTime, mass, dragCoefficient, drivingForce, drivingFrequency, plotStartTime, clamp, plotType)
+periodCounter.run(gravity, pendulumLength, initialTheta, maxTheta, thetaIncrement, initialOmega, initialTime, timeStep, maxTime, mass, dragCoefficient, drivingForce, drivingFrequency, plotStartTime, clamp, plotType)
 
 plt.legend(loc="upper right")
 plt.grid(True)
---
+++ periodCounter.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+yAxisTable = []
+xAxisTable = []
+thetaTable = []
+singlePeriodTimeTable = []
+
+
+def run(gravity, pendulumLength, initialTheta, maxTheta, thetaIncrement, initialOmega, initialTime, timeStep, maxTime, mass, dragCoefficient, drivingForce, drivingFrequency, plotStartTime, clamp, plotType):
+    currentOmega = initialOmega
+    currentTime = initialTime
+    thetaTablePopulator = initialTheta
+    naturalFrequency = np.sqrt(gravity/pendulumLength)
+    dragFactor = dragCoefficient / mass
+    drivingAngularAcceleration = mass * pendulumLength * drivingForce
+    currentPeriodStartTime = 0
+    totalPeriodTime = 0.0
+
+    while thetaTablePopulator <= maxTheta:
+        thetaTable.append(thetaTablePopulator)
+        thetaTablePopulator = thetaTablePopulator + thetaIncrement
+    for i in range(0, len(thetaTable)):
+        currentTheta = thetaTable[i]
+        while currentTime <= maxTime:
+            currentAlpha = (gravity * currentTheta) / pendulumLength
+            lastOmega = currentOmega
+            currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragFactor * currentOmega + drivingAngularAcceleration * np.sin(drivingFrequency * currentTime)) * timeStep
+            currentTheta = currentTheta + currentOmega * timeStep
+            currentEnergy = 0.5 * mass * pendulumLength**2 * currentOmega**2 + 0.5 * mass * gravity * pendulumLength * currentTheta**2
+            currentTime = currentTime + timeStep
+
+            if (lastOmega > 0 > currentOmega) or (lastOmega < 0 < currentOmega):
+                currentPeriodTime = currentTime - currentPeriodStartTime
+                singlePeriodTimeTable.append(currentPeriodTime)
+                currentPeriodStartTime = currentTime
+
+
+            if clamp == True:
+                if currentTheta > np.pi:
+                    currentTheta = currentTheta - 2 * np.pi
+                elif currentTheta < - np.pi:
+                    currentTheta = currentTheta + 2 * np.pi
+
+            if plotType == "energy":  # this block deals with allowing the graph axis labels and legend labels to update automatically
+                if currentTime > plotStartTime:
+                    yAxisTable.append(currentEnergy)
+                    xAxisTable.append(currentTime)
+            elif plotType == "angle":
+                if currentTime > plotStartTime:
+                    yAxisTable.append(currentTheta)
+                    xAxisTable.append(currentTime)
+            elif plotType == "velocity":
+                if currentTime > plotStartTime:
+                    yAxisTable.append(np.abs(currentOmega))
+                    xAxisTable.append(currentTime)
+            elif plotType == "acceleration":
+                if currentTime > plotStartTime:
+                    yAxisTable.append(currentAlpha)
+                    xAxisTable.append(currentTime)
+            elif plotType == "phaseSpace":
+                if currentTime > plotStartTime:
+                    yAxisTable.append(currentOmega)
+                    xAxisTable.append(currentTheta)
+            else:
+                exit("Error: '" + str(plotType) + "' is not a valid plot type!")
+        currentTime = initialTime
+        currentPeriodStartTime = currentTime
+    #plt.plot(xAxisTable, yAxisTable, 'b.', ms=1.25, label=plotType) # plots with points instead of a line
+    plt.plot(xAxisTable, yAxisTable, label=plotType)
+
+    for i in range(0, len(singlePeriodTimeTable)):
+        print("period " + str(i) + " was " + str(singlePeriodTimeTable[i]) + " seconds.")
+        totalPeriodTime = totalPeriodTime + singlePeriodTimeTable[i]
+    averagePeriodTime = totalPeriodTime / len(singlePeriodTimeTable)
+    print("\n")
+    print("average period is: " + str(averagePeriodTime))
