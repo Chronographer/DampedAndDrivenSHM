8d82434d9de2f069a2c9b1c324bd47e52888c800
Daniel <djisenberg2015@gmail.com>
2020-05-17 11:27:55 -0400

    added the handlePlotType() function to shm_driven.py, mirroring amplitude_dependant_anharmonic.py

--- shm_driven.py
+++ shm_driven.py
@@ -31,31 +31,31 @@ def run(gravity, pendulumLength, initialTheta, initialOmega, initialTime, timeSt
             elif currentTheta < - np.pi:
                 currentTheta = currentTheta + 2 * np.pi
 
-        if plotType == "energy":  # this block allows the graph axis labels and legend labels to update automatically
-            if currentTime > plotStartTime:
-                yAxisList.append(currentEnergy)
-                xAxisList.append(currentTime)
-        elif plotType == "angle":
-            if currentTime > plotStartTime:
-                yAxisList.append(currentTheta)
-                xAxisList.append(currentTime)
-        elif plotType == "velocity":
-            if currentTime > plotStartTime:
-                yAxisList.append(np.abs(currentOmega))
-                xAxisList.append(currentTime)
-        elif plotType == "acceleration":
-            if currentTime > plotStartTime:
-                yAxisList.append(currentAlpha)
-                xAxisList.append(currentTime)
-        elif plotType == "phaseSpace":
-            if currentTime > plotStartTime:
-                yAxisList.append(currentOmega)
-                xAxisList.append(currentTheta)
-        else:
-            exit("Error: '" + str(plotType) + "' is not a valid plot type!")
+        if currentTime > plotStartTime:
+            handlePlotType(plotType, currentTime, currentEnergy, currentTheta, currentOmega, currentAlpha)
+
     plt.plot(xAxisList, yAxisList, 'b.', ms=1.25, label=plotType)
     #plt.plot(xAxisList, yAxisList, label=plotType)
 
+
+def handlePlotType(plotType, currentTime, currentEnergy, currentTheta, currentOmega, currentAlpha):  # this makes the graph axis labels and legend labels automatically change to reflect what is actually being plotted
+    if plotType == "energy":
+        yAxisList.append(currentEnergy)
+        xAxisList.append(currentTime)
+    elif plotType == "angle":
+        yAxisList.append(currentTheta)
+        xAxisList.append(currentTime)
+    elif plotType == "velocity":
+        yAxisList.append(np.abs(currentOmega))
+        xAxisList.append(currentTime)
+    elif plotType == "acceleration":
+        yAxisList.append(currentAlpha)
+        xAxisList.append(currentTime)
+    elif plotType == "phaseSpace":
+        yAxisList.append(currentOmega)
+        xAxisList.append(currentTheta)
+    else:
+        exit("Error: '" + str(plotType) + "' is not a valid plot type!")
 
 """def separatrix(theta):
     w = """
