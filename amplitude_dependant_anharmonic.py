fixed the formula error involving drag and driving force on all remaining scripts.

amplitude_dependant_anharmonic.py
@@ -17,8 +17,6 @@ def run(gravity, pendulumLength, initialTheta, maxTheta, thetaIncrement, initial
     currentTime = initialTime
     thetaListPopulator = initialTheta
     naturalFrequency = np.sqrt(gravity / pendulumLength)
-    dragFactor = dragCoefficient / mass
-    drivingAngularAcceleration = mass * pendulumLength * drivingForce
 
     while thetaListPopulator <= maxTheta:
         thetaList.append(thetaListPopulator)
@@ -35,7 +33,7 @@ def run(gravity, pendulumLength, initialTheta, maxTheta, thetaIncrement, initial
         print("current theta is: " + str(currentTheta))
         while currentTime <= maxTime:
             currentAlpha = (gravity * currentTheta) / pendulumLength
-            currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragFactor * currentOmega + drivingAngularAcceleration * np.sin(drivingFrequency * currentTime)) * timeStep
+            currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragCoefficient * currentOmega + drivingForce * np.sin(drivingFrequency * currentTime)) * timeStep
             currentTheta = currentTheta + currentOmega * timeStep
             currentEnergy = 0.5 * mass * pendulumLength**2 * currentOmega**2 + 0.5 * mass * gravity * pendulumLength * currentTheta**2
             currentTime = currentTime + timeStep

animatedDriven.py
@@ -10,11 +10,11 @@ def data_gen(currentTime=0):
         gravity = 9.8
         pendulumLength = 1.0
         timeStep = 0.1
-        dragFactor = 1.0
-        drivingAngularAcceleration = 10.73
+        dragCoefficient = 1.0
+        drivingForce = 10.73
         drivingFrequency = 0.54
         naturalFrequency = np.sqrt(gravity / pendulumLength)
-        currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragFactor * currentOmega + drivingAngularAcceleration * np.sin(drivingFrequency * currentTime)) * timeStep
+        currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragCoefficient * currentOmega + drivingForce * np.sin(drivingFrequency * currentTime)) * timeStep
         currentTheta = currentTheta + currentOmega * timeStep
         currentTime = currentTime + timeStep
 

animated_pendulum.py
@@ -67,15 +67,13 @@ def run(gravity, pendulumLength, initialTheta, initialOmega, initialTime, timeSt
     currentTheta = initialTheta
     currentOmega = initialOmega
     naturalFrequency = np.sqrt(gravity / pendulumLength)
-    dragFactor = dragCoefficient / mass
-    drivingAngularAcceleration = mass * pendulumLength * drivingForce
     lastTheta = 0
     pendulum.rotate(origin=vector(0, 0, 0), angle=currentTheta, axis=vector(0, 0, 1))
     vpython.sleep(1)
 
     while currentTime <= maxTime:
         currentAlpha = (gravity * currentTheta) / pendulumLength
-        currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragFactor * currentOmega + drivingAngularAcceleration * np.sin(drivingFrequency * currentTime)) * timeStep
+        currentOmega = currentOmega + (-naturalFrequency**2 * np.sin(currentTheta) - dragCoefficient * currentOmega + drivingForce * np.sin(drivingFrequency * currentTime)) * timeStep
         lastTheta = currentTheta
         currentTheta = currentTheta + currentOmega * timeStep
         currentEnergy = 0.5 * mass * pendulumLength**2 * currentOmega**2 + 0.5 * mass * gravity * pendulumLength * currentTheta**2